0027c0031f513eeae91d2722888d00cdc5c706f5
analuizaypessoa <analuiza.ypessoa@gmail.com>
2018-05-06 10:40:03 -0300

    s Please enter the commit message for your changes. Lines starting

--- shinylecture1/app.R
+++ shinylecture1/app.R
@@ -1,50 +1,92 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
+##################################################################################################
+#__________________________________SHINY_________________________________________________________
 
 library(shiny)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Old Faithful Geyser Data"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
-      ),
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
+  
+  # Application title
+  titlePanel("First Shiny App"),
+  
+  # Sidebar with a slider input for number of bins 
+  sidebarLayout(
+    sidebarPanel(
+      selectizeInput("name", label = "Choose stocks to compare returns:", 
+                     choices = c("Cisco", "Amazon", "Nasdaq", "MicroStrategy", "S&P500", "Apple", "IBM"),
+                     multiple = T,
+                     selected = "Cisco",
+                     options = list(maxItems = 2, placeholder = 'select a name')
       )
-   )
+      
+    ),
+    
+    # Show a plot of the generated distribution
+    mainPanel(
+      
+      plotlyOutput("trendstocks"),
+      br(),
+      br(),
+      plotlyOutput("pricestocks"),
+      dataTableOutput("dataf")
+    )
+  )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
+  
+  output$dataf <- renderDataTable(df)
+  
+  output$trendstocks <- renderPlotly({
+    if( length(input$name) == 0){
+      print("Please select at least one stock")
+    }
+    else{
+      print(input$name)
+      df_trend <- returnsdf[, c("date", input$name)]
+      print(colnames(df_trend))
       
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+      if (ncol(df_trend) == 2){
+        p <-  plot_ly(df_trend,x =~ date, y = df_trend[,2], name = input$name, mode = "lines", type = "scatter") %>%
+          layout(title= "Stock Returns")
+        p
+      } 
+      else{
+        p <- plot_ly(df_trend,x =~ date, y = df_trend[,2], name = colnames(df_trend)[2], mode = "lines", type = "scatter") %>%  
+          add_trace(y = df_trend[,3], name = colnames(df_trend)[3], mode = "lines") %>%
+          layout(title = "Stock Returns")
+        p
+      }}})
+  
+  output$pricestocks <- renderPlotly({
+    if( length(input$name) == 0){
+      print("Please select at least one stock")
+    }
+    else{
+      print(input$name)
+      df_trend <- df[, c("date", input$name)]
+      print(colnames(df_trend))
+      
+      if (ncol(df_trend) == 2){
+        p <-  plot_ly(df_trend,x =~ date, y = df_trend[,2], color = "purple", name = input$name, mode = "lines", type = "scatter") %>%
+          layout(title= "Stock Prices")
+        p
+      } 
+      else{
+        p <- plot_ly(df_trend,x =~ date, color = "purple", y = df_trend[,2], name = colnames(df_trend)[2], mode = "lines", type = "scatter") %>%  
+          add_trace(y = df_trend[,3], name = colnames(df_trend)[3], color = "green", mode = "lines") %>%
+          layout(title = "Stock Prices")
+        p
+      }}})
+  
 }
 
 # Run the application 
 shinyApp(ui = ui, server = server)
 
+#LINKS UTEIS
+#https://shiny.rstudio.com/
+#https://plot.ly/r/shiny-gallery/
+#https://moderndata.plot.ly/dashboards-in-r-with-shiny-plotly/
+#https://rstudio.github.io/shinydashboard/
